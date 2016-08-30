ac15d5f508e1dfafe6a0c58a5211c2486232877d
Relu Negruser <relu.negruser@thinslices.com>
2016-08-30 11:35:00 +0300

    Referenced file to fix error

--- vendingmachine/js/app.js
+++ vendingmachine/js/app.js
@@ -1,7 +1,8 @@
-var machine = new VendingMachine();
 var VendingMachine = (function () {
     function VendingMachine() {
     }
     return VendingMachine;
 }());
+/// <reference path="VendingMachine" />
+var machine = new VendingMachine();
 //# sourceMappingURL=app.js.map
--- vendingmachine/ts/bootstrapper.ts
+++ vendingmachine/ts/bootstrapper.ts
@@ -1,1 +1,3 @@
+/// <reference path="VendingMachine" />
+
 var machine = new VendingMachine();
