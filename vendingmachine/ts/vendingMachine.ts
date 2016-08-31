7c23b626b75a24f5305213dc0297db2d3c5f3980
Relu Negruser <relu.negruser@thinslices.com>
2016-08-31 09:42:42 +0300

    Introduced knouckout.js

--- vendingmachine/ts/vendingMachine.ts
+++ vendingmachine/ts/vendingMachine.ts
@@ -1,10 +1,11 @@
 /// <reference path="./coin.ts" />
 
 class VendingMachine {
-    private paid = 0;
-    public acceptCoin = (coin: Quarter): void => {
-        this.paid = this.paid + coin.Value;
-        var element = document.getElementById("total");
-        element.innerHTML = this.paid.toString();   
+    private paid = ko.observable(0);
+    acceptCoin = (coin: Quarter): void => {
+        let oldTotal = this.paid();
+        this.paid(oldTotal + coin.Value);
     }
 }
+
+machine = new VendingMachine();
